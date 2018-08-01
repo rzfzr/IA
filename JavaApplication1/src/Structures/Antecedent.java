4a0bd0b7ee2f670048db344ae9706ac5a78274c2
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2018-08-01 17:30:40 -0300

    Ta no git

---
+++ JavaApplication1/src/Structures/Antecedent.java
@@ -0,0 +1,19 @@
+package Structures;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rzfzr
+ */
+public class Antecedent {
+
+    private Variable variable;
+    private Value value;
+    private boolean not;
+    private boolean relationship;
+}
---
+++ JavaApplication1/src/Structures/Variable.java
@@ -0,0 +1,23 @@
+package Structures;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rzfzr
+ */
+public class Variable {
+    
+    private boolean objective;
+    private String identification;
+    private String name;    
+    private String type;
+//    private 
+//    private WorkMemory workmemory;
+
+
+}
